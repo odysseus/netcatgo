bb904d4f8c9fd1722f207fa0c427fd482b4d6204
Ryan Case <rmcase@gmail.com>
2014-12-20 17:28:32 -0700

    Refactored some of the communication methods to use the net.Conn interface, removed some redundant code

--- netcat.go
+++ netcat.go
@@ -4,13 +4,42 @@ import (
   "net"
   "fmt"
   "bufio"
+  "log"
   "os"
+  "io"
 )
 
 func main() {
-  conn, err := UDPConnect("localhost:5050", ":4040")
-  if err != nil { fmt.Println(err); return }
-  UDPCommunicate(conn)
+  conn, err := UDPConnect(":5050", ":4040")
+  if err != nil { log.Fatal(err) }
+  Communicate(conn)
+}
+
+func Listen(conn net.Conn) {
+  var buf[1024] byte
+  for {
+    rlen, err := conn.Read(buf[:])
+    if err != nil { fmt.Println(err) }
+
+    s := string(buf[:rlen])
+    fmt.Print(s)
+  }
+}
+
+func Transmit(conn net.Conn) {
+  reader := bufio.NewReader(os.Stdin)
+  for {
+    input, err := reader.ReadBytes('\n')
+    if err != nil { fmt.Println(err) }
+
+    conn.Write(input)
+  }
+}
+
+func Communicate(conn net.Conn) {
+  defer conn.Close()
+  go Listen(conn)
+  Transmit(conn)
 }
 
 func UDPConnect(locaddr, remaddr string) (*net.UDPConn, error) {
@@ -29,27 +58,38 @@ func UDPConnect(locaddr, remaddr string) (*net.UDPConn, error) {
   return conn, nil
 }
 
-func UDPListen(conn *net.UDPConn) {
-  var buf[1024] byte
-  for {
-    rlen, err := conn.Read(buf[:])
-    if err != nil { fmt.Println(err) }
+func TCPConnect(laddr, raddr string) (*net.TCPConn, error) {
+  // Resolve the local address
+  lad, err := net.ResolveTCPAddr("tcp", laddr)
+  if err != nil { return nil, err }
 
-    s := string(buf[:rlen])
-    fmt.Print(s)
-  }
+  // Resolve the remote address
+  rad, err := net.ResolveTCPAddr("tcp", raddr)
+  if err != nil { return nil, err }
+
+  // Make the connection
+  conn, err := net.DialTCP("tcp", lad, rad)
+  if err != nil { return nil, err }
+
+  return conn, nil
 }
 
-func UDPTransmit(conn *net.UDPConn) {
-  reader := bufio.NewReader(os.Stdin)
-  for {
-    input, err := reader.ReadBytes('\n')
-    if err != nil { fmt.Println(err) }
-    conn.Write(input)
-  }
-}
+func TCPListener(laddr string) {
+  l, err := net.Listen("tcp", laddr)
+  if err != nil { log.Fatal(err) }
 
-func UDPCommunicate(conn *net.UDPConn) {
-  go UDPListen(conn)
-  UDPTransmit(conn)
+  defer l.Close()
+  for {
+    // Wait for a connection
+    conn, err := l.Accept()
+    if err != nil { log.Fatal(err) }
+
+    // Handle connections in a new goroutine
+    go func(c net.Conn) {
+      // Echo incoming data
+      io.Copy(c, c)
+      // Close the connection
+      c.Close()
+    }(conn)
+  }
 }
